e1b0b24f08d63e367b344a1882f17069bcedd27c
je-ya <psychicartil@gmail.com>
2025-04-14 15:24:44 +0900

    [Fix]트럭 프리팹 수정

--- Assets/Scripts/LYR/Boss/EnemyMove.cs
+++ Assets/Scripts/LYR/Boss/EnemyMove.cs
@@ -45,8 +45,9 @@ public class EnemyMove : MonoBehaviour
     void FixedUpdate()
     {
         // 타겟이 없거나, 타겟이 파괴되었거나, 타겟이 10 유닛 이상 멀어졌을 때 새로운 타겟 검색
-        if (!isLockedOnTarget || nearestObstacle == null || IsTargetTooFar())
+        if (!isLockedOnTarget || nearestObstacle == null || IsTargetTooFar() || IsTargetTooClose())
         {
+            {
             timer += Time.deltaTime;
             if (timer >= searchInterval)
             {
@@ -209,7 +210,7 @@ public class EnemyMove : MonoBehaviour
 
         _enemyontroller.enemyTrashList.Clear();
         isShooting = false;
-    }
+    }}
 
     private void OnCollisionEnter(Collision collision)
     {
